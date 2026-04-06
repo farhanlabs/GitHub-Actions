c1e5ea0e723d44d0493a456aa59f979b4108fc97
Farhan Raza <mdfarhanraza001@gmail.com>
2026-04-06 15:12:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask import Flask
 # Flask app initialize ki
 app = Flask(__name__)
 
-# Home route (Main page)
+
+# Rule: Function se pehle 2 khali lines honi chahiye (E302 fix)
 @app.route('/')
 def home():
     return "Hello! Ye mera simple portfolio server hai."
 
-# Server ko run karne ke liye code
+
+# Rule: Function ke baad 2 khali lines honi chahiye (E305 fix)
 if __name__ == '__main__':
     # host='0.0.0.0' taaki ye network par access ho sake
     app.run(debug=True, host='0.0.0.0', port=5000)
